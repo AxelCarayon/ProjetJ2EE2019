813d969fe5b3ef0afccbb6a98816bce13c3f8479
AxelCarayon <axel.carayon@etud.univ-jfc.fr>
2019-11-26 09:15:09 +0100

    Intégration de la base de données (embedded)

--- src/main/java/modele/dao/DAOclient.java
+++ src/main/java/modele/dao/DAOclient.java
@@ -5,6 +5,11 @@
  */
 package modele.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.LinkedList;
 import java.util.List;
 import javax.sql.DataSource;
 import modele.entity.ClientEntity;
@@ -26,9 +31,30 @@ public class DAOclient {
 		this.myDataSource = dataSource;
 	}
         
-        public List<ClientEntity> tousLesClients(){
-            //TODO
-            return null;
+        public List<ClientEntity> tousLesClients() throws SQLException{
+            List<ClientEntity> result = new LinkedList<>();
+
+		String sql = "SELECT * FROM CLIENT";
+		try (Connection connection = myDataSource.getConnection();
+                     PreparedStatement stmt = connection.prepareStatement(sql)) {
+                        ResultSet rs = stmt.executeQuery();
+			while (rs.next()) {
+                            String code = rs.getString("CODE");
+                            String societe = rs.getString("SOCIETE");
+                            String contact = rs.getString("CONTACT");
+                            String fonction = rs.getString("FONCTION");
+                            String adresse = rs.getString("ADRESSE");
+                            String ville = rs.getString("VILLE");
+                            String region = rs.getString("REGION");
+                            String code_postal = rs.getString("CODE_POSTAL");
+                            String pays = rs.getString("PAYS");
+                            String telephone = rs.getString("TELEPHONE");
+                            String fax = rs.getString("FAX");
+                            ClientEntity c = new ClientEntity(code,societe,contact,fonction,adresse,ville,region,code_postal,pays,telephone,fax);
+                            result.add(c);
+			}
+		}
+		return result;
         }
         
         public ClientEntity afficherClient(String code){
